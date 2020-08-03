02504c2cf6e24fd17cb56f155ebb0dae6481e2b9
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-08-03 12:46:12 -0300

    configurar o knexfile

---
+++ knexfile.js
@@ -0,0 +1,53 @@
+// Update with your config settings.
+
+module.exports = {
+
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: './__tests__/database.sqlite'
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/src/database/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`
+    }
+  },
+
+  development: {
+    client: 'mysql',
+    connection: {
+      host: process.env.DB_HOST || '127.0.0.1',
+      database: process.env.DB_NAME || 'voting-app',
+      user: process.env.DB_USER || 'root',
+      password: process.env.DB_PASSOWRD || 'root'
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/src/database/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`
+    }
+  },
+
+  production: {
+    client: 'mysql',
+    connection: {
+      host: process.env.DB_HOST || '127.0.0.1',
+      database: process.env.DB_NAME || 'voting-app',
+      user: process.env.DB_USER || 'root',
+      password: process.env.DB_PASSOWRD || 'root'
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/src/database/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`
+    }
+  }
+
+}
