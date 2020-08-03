75fcfd54e94a97b515b230895423df060ebf2a12
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-08-03 12:34:59 -0300

    corrigir identação do código

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ const express = require('express')
 require('dotenv').config()
 
 class AppController {
-    constructor () {
-        this.express = express()
-        this.middlewares = this.middlewares()
-        this.routes = this.routes()
-    }
+  constructor () {
+    this.express = express()
+    this.middlewares = this.middlewares()
+    this.routes = this.routes()
+  }
 
-    middlewares () {
-        this.express.use(express.json())
-    }
+  middlewares () {
+    this.express.use(express.json())
+  }
 
-    routes () {
+  routes () {
 
-    }
+  }
 }
 
 module.exports = new AppController().express
--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ const app = require('./index')
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
+  console.log(`Server listening on port ${PORT}`)
 })
