acc0d7036217ebc34a9282f18f221cba547b180e
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-08-03 12:30:33 -0300

    configurar o servidor express

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+require('dotenv').config()
+
+class AppController {
+    constructor () {
+        this.express = express()
+        this.middlewares = this.middlewares()
+        this.routes = this.routes()
+    }
+
+    middlewares () {
+        this.express.use(express.json())
+    }
+
+    routes () {
+
+    }
+}
+
+module.exports = new AppController().express
