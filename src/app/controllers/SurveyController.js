485206b4e4df10c8eb95634bbdd75f752e9fcc04
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-08-05 17:04:46 -0300

    usar o model survey no controller

--- src/app/controllers/SurveyController.js
+++ src/app/controllers/SurveyController.js
@@ -1,28 +1,13 @@
-const knex = require('../../database/index')
+const Survey = require('../models/Survey')
 
 class SurveyController {
   async create (req, res) {
-    const surveyId = await knex('surveys').insert({
-      title: req.body.title,
-      description: req.body.description
-    })
-
-    req.body.options.forEach(async option => {
-      await knex('options').insert({ survey_id: surveyId, name: option.name })
-    })
-
-    const survey = await knex('surveys').where({ id: surveyId })
-    const options = await knex('options').where({ survey_id: surveyId })
-    const a = {
-      id: survey[0].id,
-      title: survey[0].title,
-      description: survey[0].description,
-      created_at: survey[0].created_at,
-      updated_at: survey[0].updated_at,
-      options: options
+    try {
+      const survey = await Survey.create(req.body)
+      return res.json({ survey })
+    } catch (error) {
+      return res.status(500).json({ error: 'internal error' })
     }
-
-    return res.json({ survey: a })
   }
 }
 
