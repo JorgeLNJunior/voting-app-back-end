0ddafea82a1cd3e7feba603711dfe20fd409518f
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-08-05 18:07:55 -0300

    refatorar a validação

--- src/app/controllers/SurveyController.js
+++ src/app/controllers/SurveyController.js
@@ -1,25 +1,11 @@
 const Survey = require('../models/Survey')
+const validator = require('../validators/SurveyValidator')
 
 class SurveyController {
   async create (req, res) {
-    const errors = []
-    if (!req.body.title) {
-      errors.push({ error: 'field title is required' })
-    }
-    if (!req.body.description) {
-      errors.push({ error: 'field description is required' })
-    }
-    if (!req.body.options || req.body.options.length <= 0) {
-      errors.push({ error: 'field options cannot be empty' })
-    } else {
-      req.body.options.forEach(option => {
-        if (!option.name) {
-          errors.push({ error: 'field option name cannot be empty' })
-        }
-      })
-    }
-    if (errors.length > 0) {
-      return res.status(400).json({ errors })
+    const validation = validator.validateCreate(req.body)
+    if (!validation.pass) {
+      return res.status(400).json({ errors: validation.errors })
     }
     try {
       const survey = await Survey.create(req.body)
