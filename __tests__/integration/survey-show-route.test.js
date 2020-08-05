53352e5cef8122270ad15f0aa572669fbb187d8e
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-08-05 19:02:51 -0300

    adicionar triggers nos teste da rota show surveys

--- __tests__/integration/survey-show-route.test.js
+++ __tests__/integration/survey-show-route.test.js
@@ -1,8 +1,12 @@
 const request = require('supertest')
 const Survey = require('../../src/app/models/Survey')
 const app = require('../../src/app')
+const dbUtil = require('../utils/dbUtil')
 
 describe('Show survey route', () => {
+  beforeEach(async () => await dbUtil.cleanTables())
+  afterAll(async () => await dbUtil.destroyConnection())
+
   it('should return a object with survey', async () => {
     const survey = await Survey.create({
       title: 'title',
