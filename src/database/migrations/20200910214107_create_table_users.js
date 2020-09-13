1e0f50e812da7bc3aad726e16c71e58a0f360b5d
JorgeJr <jorgelnjunior@gmail.com>
2020-09-13 18:02:42 -0300

    remover o limite do campo password

--- src/database/migrations/20200910214107_create_table_users.js
+++ src/database/migrations/20200910214107_create_table_users.js
@@ -4,7 +4,7 @@ exports.up = function (knex) {
     table.increments('id')
     table.string('name').notNullable()
     table.string('email').notNullable().unique()
-    table.string('password', 20).notNullable()
+    table.string('password').notNullable()
     table.timestamp('created_at').defaultTo(knex.fn.now())
     table.timestamp('updated_at').defaultTo(knex.fn.now())
   })
