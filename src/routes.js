7b36ff670488e2dda7b5f28ac7e35197ee9c4268
JorgeJr <jorgelnjunior@gmail.com>
2020-09-16 15:54:40 -0300

    usar middleware de autenticação nas rotas

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 const router = require('express').Router()
 const SurveyController = require('./app/controllers/SurveyController')
 const AuthController = require('./app/controllers/AuthController')
+const AuthMiddleware = require('./app/middlewares/AuthMiddleware')
 
 router.post('/register', AuthController.register)
 router.post('/login', AuthController.login)
+
+router.use(AuthMiddleware)
 
 router.post('/surveys', SurveyController.create)
 router.get('/surveys/:id', SurveyController.show)
