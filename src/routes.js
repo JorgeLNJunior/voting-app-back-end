1f577699fef99e0a6c0776ad39b848c49a8bff48
JorgeJr <jorgelnjunior@gmail.com>
2020-09-18 16:07:59 -0300

    criar rota para retorno de usuário

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const SurveyController = require('./app/controllers/SurveyController')
 const AuthController = require('./app/controllers/AuthController')
 const AuthMiddleware = require('./app/middlewares/AuthMiddleware')
+const User = require('./app/models/User')
 
 router.post('/register', AuthController.register)
 router.post('/login', AuthController.login)
@@ -10,7 +11,11 @@ router.use(AuthMiddleware)
 
 router.post('/surveys', SurveyController.create)
 router.get('/surveys/:id', SurveyController.show)
-
 router.post('/surveys/:surveyId/vote/:optionId', SurveyController.addVote)
+
+router.get('/users/:id', async (req, res) => {
+  const user = await User.getByID(req.params.id)
+  return res.json({ user: user })
+})
 
 module.exports = router
