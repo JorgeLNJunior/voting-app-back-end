c5082d58a9a3c7be33385f3f87bd79d52f08db2a
JorgeJr <jorgelnjunior@gmail.com>
2020-09-19 12:42:42 -0300

    criar teste para retornar dados do usuário atualizado

--- __tests__/integration/user/update-user-route.test.js
+++ __tests__/integration/user/update-user-route.test.js
@@ -8,7 +8,7 @@ describe('update user route', () => {
   beforeEach(async () => await dbUtil.cleanTables())
   afterAll(async () => await dbUtil.destroyConnection())
 
-  it('should 200 if user has been updated', async () => {
+  it('should return 200 if the user has been updated', async () => {
     const user = await Factory.createUser()
     const token = AuthService.generateToken(user.id)
 
@@ -18,5 +18,17 @@ describe('update user route', () => {
       .set('Authorization', `Bearer ${token}`)
 
     expect(response.status).toBe(200)
+  })
+
+  it('should return user data if the user has been updated', async () => {
+    const user = await Factory.createUser()
+    const token = AuthService.generateToken(user.id)
+
+    const response = await request(app)
+      .put(`/users/${user.id}`)
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+
+    expect(response.body).toHaveProperty('user')
   })
 })
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const SurveyController = require('./app/controllers/SurveyController')
 const AuthController = require('./app/controllers/AuthController')
 const UserController = require('./app/controllers/UserController')
 const AuthMiddleware = require('./app/middlewares/AuthMiddleware')
+const User = require('./app/models/User')
 
 router.post('/register', AuthController.register)
 router.post('/login', AuthController.login)
@@ -14,5 +15,9 @@ router.get('/surveys/:id', SurveyController.show)
 router.post('/surveys/:surveyId/vote/:optionId', SurveyController.addVote)
 
 router.get('/users/:id', UserController.getByID)
+router.put('/users/:id', async (req, res) => {
+  const user = await User.update(req.UID, { name: 'new name' })
+  return res.json({ user: user })
+})
 
 module.exports = router
