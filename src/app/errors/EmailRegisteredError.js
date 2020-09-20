11afda1e82c9dccdeaa058a786c796c8188553d1
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-09-20 16:44:55 -0300

    deletar os antigos erros

--- src/app/errors/EmailRegisteredError.js
+++
@@ -1,7 +0,0 @@
-module.exports = class EmailRegisteredError extends Error {
-  constructor () {
-    super('email is alredy registered')
-    this.name = 'EmailRegisteredError'
-    this.statusCode = 400
-  }
-}
--- src/app/errors/FieldLengthError.js
+++
@@ -1,7 +0,0 @@
-module.exports = class FiedlLengthError extends Error {
-  constructor (field, maxLength) {
-    super(`${field} field length is greater than ${maxLength}`)
-    this.name = 'FieldLengthError'
-    this.statusCode = 400
-  }
-}
