f640d38d8f4b46738208b9165b01d02fc66fb21e
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-10-11 12:21:22 -0300

    implementar o graceful shutdown

--- src/server.js
+++ src/server.js
@@ -1,7 +1,19 @@
 const app = require('./app')
+const knex = require('./database/index')
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
+})
+
+process.on('SIGTERM', () => {
+  console.info('SIGTERM signal received')
+  server.close(async () => {
+    console.log('HTTP server closed')
+    knex.destroy(() => {
+      console.log('database disconected')
+      process.exit(0)
+    })
+  })
 })
