22c83b5eb30bfc130019eea5aba02224c7a6a289
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-10-24 11:47:17 -0300

    criar teste para senha incorreta

--- __tests__/integration/user/update-password-route.test.js
+++ __tests__/integration/user/update-password-route.test.js
@@ -24,4 +24,20 @@ describe('update password route', () => {
 
     expect(response.status).toBe(200)
   })
+
+  it('should return 400 if password is invalid', async () => {
+    const user = await Factory.createUser()
+    const token = AuthService.generateToken(user.id)
+
+    const response = await request(app)
+      .post(`/users/${user.id}/password`)
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        oldPassword: 'wrongPassword',
+        newPassword: 'password'
+      })
+
+    expect(response.status).toBe(400)
+  })
 })
