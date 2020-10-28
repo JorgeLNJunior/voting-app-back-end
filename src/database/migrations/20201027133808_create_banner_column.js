ef745ab6dd40488b72810abcac832b1f8acf16a2
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-10-28 13:57:24 -0300

    adicionar coluna banner a tabela surveys

--- src/database/migrations/20201027133808_create_banner_column.js
+++ src/database/migrations/20201027133808_create_banner_column.js
@@ -1,7 +1,7 @@
 
 exports.up = function (knex) {
   return knex.schema.table('surveys', table => {
-    table.string('banner').defaultTo('https://picsum.photos/1280/720')
+    table.string('banner')
   })
 }
 
