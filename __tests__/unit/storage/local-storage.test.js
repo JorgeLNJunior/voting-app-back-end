c38dcdc84a631f1e798d0cd7c3237e1261dcd44d
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-11-02 14:00:24 -0300

    corrigir o teste do local storage

--- __tests__/unit/storage/local-storage.test.js
+++ __tests__/unit/storage/local-storage.test.js
@@ -1,10 +1,11 @@
 const LocalStorage = require('../../../src/app/services/storage/LocalStorage')
 const fs = require('fs')
 const banner = fs.readFileSync(`${__dirname}/../../helpers/images/banner.jpg`) // eslint-disable-line
+const avatar = fs.readFileSync(`${__dirname}/../../helpers/images/avatar.jpg`) // eslint-disable-line
 
 describe('local storage', () => {
   it('should return a local avatar url', async () => {
-    const imgData = { buffer: Buffer.from(banner, 'binary'), originalname: 'banner.jpg' }
+    const imgData = { buffer: Buffer.from(avatar, 'binary'), originalname: 'banner.jpg' }
     const url = await LocalStorage.storeUserAvatar(imgData)
 
     expect(url).toContain('http://0.0.0.0:3000/uploads/avatars/')
