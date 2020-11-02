61107dcfbfe0f25a02908c4473979b3467b4b8f1
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-11-02 14:44:04 -0300

    criar teste para usuário não autorizado

--- __tests__/integration/survey/survey-banner-update-route.test.js
+++ __tests__/integration/survey/survey-banner-update-route.test.js
@@ -24,4 +24,20 @@ describe('survey', () => {
 
     expect(response.status).toBe(200)
   })
+
+  it('should return 403 if user does not have permission to update', async () => {
+    const user = await Factory.createUser()
+    const unauthorizedUser = Factory.createUser()
+    const token = AuthService.generateToken(unauthorizedUser.id)
+
+    const survey = await Factory.createSurvey(user.id)
+
+    const response = await request(app)
+      .post(`/surveys/${survey.id}/banner`)
+      .set('Content-Type', 'application/json')
+      .set('Authorization', `Bearer ${token}`)
+      .attach('banner', bannerPath)
+
+    expect(response.status).toBe(403)
+  })
 })
