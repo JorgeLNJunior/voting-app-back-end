39f073ce053153631e1cffdb3b8f9a1c1a20d0cd
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-11-02 15:06:19 -0300

    criar testes para survey iexistente e banner maior que 2mb

--- __tests__/integration/survey/survey-banner-update-route.test.js
+++ __tests__/integration/survey/survey-banner-update-route.test.js
@@ -5,6 +5,7 @@ const Factory = require('../../Factory')
 const AuthService = require('../../../src/app/services/AuthService')
 
 const bannerPath = `${__dirname}/../../helpers/images/banner.jpg` // eslint-disable-line
+const bannerNoCompressedPath = `${__dirname}/../../helpers/images/banner-no-compressed.jpg` // eslint-disable-line
 
 describe('survey', () => {
   beforeEach(async () => await dbHelper.cleanTables())
@@ -34,10 +35,38 @@ describe('survey', () => {
 
     const response = await request(app)
       .post(`/surveys/${survey.id}/banner`)
-      .set('Content-Type', 'application/json')
+      .set('Content-Type', 'multipart/form-data')
       .set('Authorization', `Bearer ${token}`)
       .attach('banner', bannerPath)
 
     expect(response.status).toBe(403)
+  })
+
+  it('should return 400 if survey does not exist', async () => {
+    const user = await Factory.createUser()
+    const token = AuthService.generateToken(user.id)
+
+    const response = await request(app)
+      .post(`/surveys/${500}/banner`)
+      .set('Content-Type', 'multipart/form-data')
+      .set('Authorization', `Bearer ${token}`)
+      .attach('banner', bannerPath)
+
+    expect(response.status).toBe(400)
+  })
+
+  it('should return 400 if banner size is greater than 2mb', async () => {
+    const user = await Factory.createUser()
+    const token = AuthService.generateToken(user.id)
+
+    const survey = await Factory.createSurvey(user.id)
+
+    const response = await request(app)
+      .post(`/surveys/${survey.id}/banner`)
+      .set('Content-Type', 'multipart/form-data')
+      .set('Authorization', `Bearer ${token}`)
+      .attach('banner', bannerNoCompressedPath)
+
+    expect(response.status).toBe(400)
   })
 })
