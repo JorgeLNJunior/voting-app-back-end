1bce6f186a8f7d8a7611de875999fef4a5879c44
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-11-03 16:55:00 -0300

    adicionar tratamento de erros do multer

--- src/app/middlewares/ErrorHandler.js
+++ src/app/middlewares/ErrorHandler.js
@@ -1,6 +1,11 @@
 const { GeneralError } = require('../helpers/Errors')
+const multer = require('multer')
 
-module.exports = function (err, req, res, next) {
+module.exports = function (err, req, res) {
+  console.log(err)
+  if (err instanceof multer.MulterError) {
+    return res.status(400).json({ error: err.message })
+  }
   if (err instanceof GeneralError) {
     return res.status(err.getCode()).json({ error: err.message })
   } else {
