d5e91eb3355c7b96dfb70702e181cfed0594632b
JorgeLNJunior <jorgelnjunior@gmail.com>
2020-11-12 11:28:49 -0300

    adicionar limite de requisições a api

--- src/app.js
+++ src/app.js
@@ -3,12 +3,20 @@ const path = require('path')
 const cors = require('cors')
 const morgan = require('mongoose-morgan')
 const helmet = require('helmet')
+const rateLimit = require('express-rate-limit')
 const errorHandler = require('./app/middlewares/ErrorHandler')
 require('dotenv').config()
 
 const app = express()
 
+const limiter = rateLimit({
+  windowMs: 60000,
+  max: process.env.NODE_ENV === 'test' ? 200 : 30,
+  message: { error: 'too many requests, please try again later' }
+})
+
 app.use(express.json())
+app.use(limiter)
 app.use(express.static(path.resolve(`${__dirname}/public`))) // eslint-disable-line
 app.use(cors())
 app.use(helmet())
